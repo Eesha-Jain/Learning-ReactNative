1d6e79a505c64672e70e973365b9d8f243a9ed66
Eesha-Jain <queenoftherace@gmail.com>
2021-06-13 20:42:06 -0700

    Implement todos and newToDo states into view

--- src/Home.js
+++ src/Home.js
@@ -1,112 +1,58 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
  */
 
+//Import statements
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { View, TextInput, Text, StyleSheet, Dimensions, TouchableHighlight } from "react-native";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+//Dimensions of the screen
+var width = Dimensions.get('window').width;
+var height = Dimensions.get('window').height;
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+//Home class
+class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: [1,2,3],
+      newTodo: ""
+    }
+  }
 
-const Home: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  handleChange(e) {
+    const { value } = e.target;
+    this.setState({newTodo: value})
+  }
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  render() {
+    return (
+      <View style={styles.sectionContainer}>
+        <TextInput style={styles.input} value={this.state.newTodo} onChange={this.handleChange.bind(this)} />
+        <TouchableHighlight>
+          <Text>Add Task</Text>
+        </TouchableHighlight>
+        {this.state.todos.map((todo, i) => <Text key={i}>{todo}</Text>)}
+      </View>
+    );
+  }
+}
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
+//Styles
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  input: {
+    backgroundColor: '#dddddd',
+    width: 100,
+    padding: 5,
+    marginBottom: 10
+  }
 });
 
+//Export
 export default Home;
